b9d4a76c66105e3f1230b69cbbc0ed6394e7c2f3
hagaitski <i.haga629@gmail.com>
2021-12-22 22:36:54 +0900

    fix: unko

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::io::{stdout, Error, Write};
+use std::io::{repeat, stdout, Error, Write};
 use std::{thread, time};
 
 use rand::{thread_rng, Rng};
@@ -9,13 +9,14 @@ const SIZE: usize = 32;
 #[derive(Clone)]
 pub struct Micron;
 
+#[derive(Eq, PartialEq)]
 pub struct Point {
     x: usize,
     y: usize,
 }
 
 impl Point {
-    pub fn move_simple(&self, dir: Direction, num: usize) -> Self {
+    pub fn move_simple(&self, dir: &Direction, num: usize) -> Self {
         match dir {
             Direction::Up => Self {
                 x: self.x,
@@ -51,43 +52,62 @@ impl Direction {
 }
 
 fn next(world: &mut World) {
-    let dirs : Vec<_> = Direction::all().collect();
+    let dirs: Vec<_> = Direction::all().collect();
     let mut rng = thread_rng();
-    let object = choose(&world.objects,&mut rng);
-    
-    let objects = world.objects.iter().map(|(micron, point)| {
-        let dir = dirs.get(rng.gen_range(0..dirs.len())).unwrap();
-    });
-    todo!()
-} 
+    let (micron, point) = pick(&mut world.objects, &mut rng);
+    let dir = choose(&dirs, &mut rng);
+    let new_one = (micron.clone(), point.move_simple(dir, 1));
+    for (_, point) in world.objects.iter_mut() {
+        let dir = choose(&dirs, &mut rng);
+        *point = point.move_simple(dir, 1);
+    }
+    world.objects.push(new_one);
+}
 
-fn choose<'a,T,R: Rng>(vec: &'a Vec<T>,rng: &mut R) -> &'a T {
+fn choose<'a, T, R: Rng>(vec: &'a Vec<T>, rng: &mut R) -> &'a T {
     vec.get(rng.gen_range(0..vec.len())).expect("NEVER")
 }
 
-fn choose_mut<'a,T,R: Rng>(vec: &'a mut Vec<T>,rng: &mut R) -> &'a mut T {
+fn pick<T, R: Rng>(vec: &mut Vec<T>, rng: &mut R) -> T {
+    let index = rng.gen_range(0..vec.len());
+    vec.remove(index)
+}
+
+fn choose_mut<'a, T, R: Rng>(vec: &'a mut Vec<T>, rng: &mut R) -> &'a mut T {
     let len = vec.len();
     vec.get_mut(rng.gen_range(0..len)).expect("NEVER")
 }
-
 
 pub struct World {
     pub objects: Vec<(Micron, Point)>,
 }
 
 fn main() -> Result<(), Error> {
+    let mut world = World {
+        objects: vec![(Micron, Point { x: 15, y: 15 })],
+    };
     let mut stdout = stdout();
+
     for i in 0..100 {
         thread::sleep(time::Duration::from_millis(100));
+        let str: String = (0..SIZE).map(|y| {
+            (0..SIZE).map(|x| {
+                let found = world
+                    .objects
+                    .iter()
+                    .find(|(_, point)| point == &Point { x, y });
+                found.and_then(|_| Some("●")).unwrap_or_else(|| " ")
+            }).chain(["\n"]) .collect::<String>()
+        }).collect();
         write!(
             stdout,
-            "{}{}res: {}\n{}",
+            "{}{}{}",
             termion::clear::All,
             termion::cursor::Goto(1, 1),
-            i,
-            " ".repeat(i) + "●"
+            str
         )?;
         stdout.flush()?;
+        next(&mut world);
     }
     Ok(())
 }
